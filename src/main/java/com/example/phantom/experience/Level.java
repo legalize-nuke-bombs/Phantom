8670a8d6cfacd836edc48abc8779ff4fd5a2c682
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-05-22 18:00:56 +0300

    update level constants

--- src/main/java/com/example/phantom/experience/Level.java
+++ src/main/java/com/example/phantom/experience/Level.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 public enum Level {
     Whisper(0L),
     Echo(500L),
-    Shade(1000L),
-    Wisp(2500L),
-    Spectre(5000L),
-    Phantom(10000L),
-    Revenant(25000L),
-    Reaper(50000L)
+    Shade(2500L),
+    Wisp(5000L),
+    Spectre(10000L),
+    Phantom(25000L),
+    Revenant(50000L),
+    Reaper(100000L)
     ;
 
     private final Long amount;
