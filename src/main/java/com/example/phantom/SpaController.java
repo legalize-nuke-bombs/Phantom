bc3e1e88b99dc32e00bc45abfa2cb491d293e0e7
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-05-25 16:41:10 +0300

    frontend support

---
+++ src/main/java/com/example/phantom/SpaController.java
@@ -0,0 +1,16 @@
+package com.example.phantom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+
+    @GetMapping(value = {
+            "/{path:[^\\.]*}",
+            "/{path:[^\\.]*}/{subpath:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
