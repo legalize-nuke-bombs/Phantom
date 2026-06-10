5e46dc4fd07b80ab740065f6a9e7cbd161afe1cd
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-10 16:17:32 +0300

    add id field to GET /api/ref

--- src/main/java/com/example/phantom/ref/RefStorageRepresentation.java
+++ src/main/java/com/example/phantom/ref/RefStorageRepresentation.java
@@ -6,10 +6,12 @@ import java.math.BigDecimal;
 
 @Getter
 public class RefStorageRepresentation {
+    private final Long id;
     private final BigDecimal amount;
     private final BigDecimal total;
 
     public RefStorageRepresentation(RefStorage rs) {
+        this.id = rs.getId();
         this.amount = rs.getAmount();
         this.total = rs.getTotal();
     }
