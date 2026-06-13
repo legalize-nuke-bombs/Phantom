4a41b62e11914b95e7ba929b3acecd909f8bf459
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-13 16:35:34 +0300

    add id field to file representation

--- src/main/java/com/example/phantom/disk/FileRepresentation.java
+++ src/main/java/com/example/phantom/disk/FileRepresentation.java
@@ -3,14 +3,18 @@ package com.example.phantom.disk;
 import com.example.phantom.profile.ProfileCardRepresentation;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class FileRepresentation {
+    private final UUID id;
     private final Long timestamp;
     private final ProfileCardRepresentation profileCard;
     private final String name;
     private final Long size;
 
     public FileRepresentation(File file, ProfileCardRepresentation profileCard) {
+        this.id = file.getId();
         this.timestamp = file.getTimestamp();
         this.profileCard = profileCard;
         this.name = file.getOriginalName();
