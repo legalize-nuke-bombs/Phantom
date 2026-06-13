7a5caa69e0e7439eecd084cde88944b9753acce5
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-13 17:01:09 +0300

    50mb -> 100 mb for base disk usage

--- src/main/java/com/example/phantom/disk/DiskSettings.java
+++ src/main/java/com/example/phantom/disk/DiskSettings.java
@@ -11,14 +11,14 @@ public class DiskSettings {
 
     public DiskSettings() {
         this.baseRule = new DiskRule(
-                50L * 1024 * 1024,
-                100,
-                100
+                100L * 1024 * 1024,
+                1000,
+                1000
         );
         this.extendedRule = new DiskRule(
                 1L * 1024 * 1024 * 1024,
-                1000,
-                1000
+                10000,
+                10000
         );
     }
 }
