91f8a842d237285532584a72c3cd1b387e03f48f
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-19 00:49:11 +0300

    sending chatId as String to avoid rounding problems

--- src/main/java/com/example/phantom/chat/chat/ChatRepresentation.java
+++ src/main/java/com/example/phantom/chat/chat/ChatRepresentation.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 @Getter
 public class ChatRepresentation {
-    private final Long id;
+    private final String id;
     private final String topicId;
     private final Long timestamp;
     private final List<UserShortRepresentation> members;
 
     public ChatRepresentation(Chat chat, List<UserShortRepresentation> members) {
-        this.id = chat.getId();
+        this.id = String.valueOf(chat.getId());
         this.topicId = chat.getTopic().getId();
         this.timestamp = chat.getTopic().getTimestamp();
         this.members = members;
--- src/main/java/com/example/phantom/chat/message/MessageRepresentation.java
+++ src/main/java/com/example/phantom/chat/message/MessageRepresentation.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class MessageRepresentation {
     private final Long id;
-    private final Long chatId;
+    private final String chatId;
     private final UserShortRepresentation user;
     private final Long timestamp;
     private final String content;
@@ -15,7 +15,7 @@ public class MessageRepresentation {
 
     public MessageRepresentation(Message message) {
         this.id = message.getId();
-        this.chatId = message.getChat().getId();
+        this.chatId = String.valueOf(message.getChat().getId());
         this.user = new UserShortRepresentation(message.getUser());
         this.timestamp = message.getTimestamp();
         this.content = message.getContent();
