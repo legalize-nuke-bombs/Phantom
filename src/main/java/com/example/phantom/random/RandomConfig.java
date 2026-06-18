5aaa7510c822108bc8f29c8bb073954ddfa26018
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-18 15:53:49 +0300

    add random bean

---
+++ src/main/java/com/example/phantom/random/RandomConfig.java
@@ -0,0 +1,14 @@
+package com.example.phantom.random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class RandomConfig {
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+}
