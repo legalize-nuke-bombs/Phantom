f7fce3f51d2d194879c78472a7b3c15f26d3297b
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-06-23 12:55:52 +0300

    update levels

--- src/main/java/com/example/phantom/experience/Level.java
+++ src/main/java/com/example/phantom/experience/Level.java
@@ -7,13 +7,13 @@ import java.util.Set;
 @Getter
 public enum Level {
     Whisper(0L),
-    Echo(500L, Set.of(LevelFeature.SEND_MESSAGE, LevelFeature.SEND_PRESENT, LevelFeature.DISK_BASE)),
-    Shade(2500L),
-    Wisp(5000L),
-    Spectre(10000L),
-    Phantom(25000L, Set.of(LevelFeature.DISK_PLUS)),
-    Revenant(50000L),
-    Reaper(100000L)
+    Echo(2500L, Set.of(LevelFeature.SEND_MESSAGE, LevelFeature.SEND_PRESENT, LevelFeature.DISK_BASE)),
+    Shade(5000L),
+    Wisp(10000L),
+    Spectre(25000L),
+    Phantom(50000L, Set.of(LevelFeature.DISK_PLUS)),
+    Revenant(100000L),
+    Reaper(200000L)
     ;
 
     private final Long amount;
