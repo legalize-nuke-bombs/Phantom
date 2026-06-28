9b3f6b0a0dbaef730fee4c88c4e46d71589bd2cb
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-06-28 13:47:35 +0300

    max message content size 1000 -> 20000

--- src/main/java/com/example/phantom/chat/message/MessageConstants.java
+++ src/main/java/com/example/phantom/chat/message/MessageConstants.java
@@ -1,7 +1,7 @@
 package com.example.phantom.chat.message;
 
 public class MessageConstants {
-    public static final int MAX_MESSAGE_CONTENT_LENGTH = 1000;
+    public static final int MAX_MESSAGE_CONTENT_LENGTH = 20000;
 
     private MessageConstants() {}
 }
